6c296ce26e1e8e3575df46b94eda51b87326a557
Sang Shin <sangshinpassion@gmail.com>
2022-04-20 15:23:00 -0400

    jenkins test

--- src/test/java/io/pivotal/workshop/CarControllerSliceTests.java
+++ src/test/java/io/pivotal/workshop/CarControllerSliceTests.java
@@ -74,8 +74,7 @@ public class CarControllerSliceTests {
         // verify
         verify(carService).addCarDetails(new Car("test", "test"));
     }
-
-    // convert object to String
+    
     public static String asJsonString(final Object obj) {
         try {
             final ObjectMapper mapper = new ObjectMapper();
